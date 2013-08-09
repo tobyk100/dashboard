331190f00b323b48c1d2d9e302ae676fa1ea6294
Patrick O'Donnell <patrick@urbanspoon.com>
2013-08-09 16:45:19 -0700

    Add unit tests for authorization

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -4,4 +4,41 @@ class ActivityTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "as guest" do
+    ability = Ability.new(User.new)
+
+    assert ability.can?(:read, Provider)
+    assert ability.can?(:read, Goal)
+    assert ability.can?(:read, Activity)
+
+    assert !ability.can?(:destroy, Provider)
+    assert !ability.can?(:destroy, Goal)
+    assert !ability.can?(:destroy, Activity)
+  end
+
+  test "as member" do
+    ability = Ability.new(create(:user))
+
+    assert ability.can?(:read, Provider)
+    assert ability.can?(:read, Goal)
+    assert ability.can?(:read, Activity)
+
+    assert !ability.can?(:destroy, Provider)
+    assert !ability.can?(:destroy, Goal)
+    assert !ability.can?(:destroy, Activity)
+  end
+
+  test "as admin" do
+    ability = Ability.new(create(:admin))
+
+    assert ability.can?(:read, Provider)
+    assert ability.can?(:read, Goal)
+    assert ability.can?(:read, Activity)
+
+    assert ability.can?(:destroy, Provider)
+    assert ability.can?(:destroy, Goal)
+    assert ability.can?(:destroy, Activity)
+  end
+
 end
