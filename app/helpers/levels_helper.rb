d9f349a1cad471544c43eda4c88a214a1e9c0574
Patrick O'Donnell <patrick@urbanspoon.com>
2013-08-14 14:57:02 -0700

    Add some  styling. Start work on building customized level urls

--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -1,2 +1,8 @@
 module LevelsHelper
+  def build_tagged_url(level, user)
+    uri =  URI.parse(level.level_url)
+    new_query_ar = URI.decode_www_form(uri.query) << ["callback_url", root_url(user)]
+    uri.query = URI.encode_www_form(new_query_ar)
+    uri.to_s
+  end
 end
