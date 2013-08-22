e0ba982f0a0bb786941fdf9cdeff2ba42054447e
dashboard-east-1b <dev@code.org>
2013-08-22 23:16:46 +0000

    get new domain and email working in prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Dashboard::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'dashboard.dev-code.org' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'code.org',
+      user_name:            'noreply@code.org',
+      password:             'lowsecurity',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
