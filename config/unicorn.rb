0c6a7ce8519e203bee46fc4b61c6ff7dba52688c
Patrick O'Donnell <patrick@urbanspoon.com>
2013-08-28 15:06:52 -0700

    update config to get capistrano deploy working as deamon

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 listen 3000 # by default Unicorn listens on port 8080
 listen "/tmp/unicorn.dashboard.sock"
 worker_processes 2 # this should be >= nr_cpus
-pid "/home/ubuntu/deploy/tmp/pids/unicorn.pid"
+pid "/home/ubuntu/apps/dashboard/shared/pids/unicorn.pid"
 timeout 60
 stderr_path "/var/log/unicorn/stderr.log"
 stdout_path "/var/log/unicorn/stdout.log"
-working_directory "/home/ubuntu/deploy"
+working_directory "/home/ubuntu/apps/dashboard/current"
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -8,6 +8,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/ubuntu/apps/dashboard/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+AS_USER=ubuntu
 action="$1"
 set -u
 
@@ -23,10 +24,18 @@ oldsig () {
         test -s $old_pid && kill -$1 `cat $old_pid`
 }
 
+run () {
+  if [ "$(id -un)" = "$AS_USER" ]; then
+    eval $1
+  else
+    su -c "$1" - $AS_USER
+  fi
+}
+
 case $action in
 start)
         sig 0 && echo >&2 "Already running" && exit 0
-        su -c "$CMD" - www-data
+        run "$CMD"
         ;;
 stop)
         sig QUIT && exit 0
@@ -39,7 +48,7 @@ force-stop)
 restart|reload)
         sig HUP && echo reloaded OK && exit 0
         echo >&2 "Couldn't reload, starting '$CMD' instead"
-        su -c "$CMD" - www-data
+        run "$CMD"
         ;;
 upgrade)
         if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
@@ -59,7 +68,7 @@ upgrade)
                 exit 0
         fi
         echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-        su -c "$CMD" - www-data
+        run "$CMD"
         ;;
 reopen-logs)
         sig USR1
