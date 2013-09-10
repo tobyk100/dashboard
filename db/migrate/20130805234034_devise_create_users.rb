1dd4997bcdd17ce21d8e35a13fcfff925d4355c4
Patrick O'Donnell <patrick@urbanspoon.com>
2013-09-10 16:03:40 -0700

    remove unique constraint on email to allow users without an email address. validation to be done in model

--- db/migrate/20130805234034_devise_create_users.rb
+++ db/migrate/20130805234034_devise_create_users.rb
@@ -37,7 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    add_index :users, :email        # Can't be unique since we allow username only users , :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
