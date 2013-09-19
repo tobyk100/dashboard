fd5e858a8d2ae039608a343e803f3ca6c6dc4636
Tobias Kahan <tobyk100@gmail.com>
2013-09-18 18:45:54 -0700

    - Build query parameters for blockly instead of the whole url. - Change next_level_url to script_level_url: Makes it a more flexible   function, a caller can always pass in next_level especially now   that the model supports that instance method.

--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -1,30 +1,14 @@
 module LevelsHelper
-  def build_tagged_url(script_level, user)
-    callback_url = ["callback_url", milestone_url(user_id: user.try(:id) || 0, script_level_id: script_level)]
+  def build_query_params(script_level, user)
     level = script_level.level
-
-    build_level_url(level, user, callback_url)
+    new_query_ar = {"level" => level.level_num || 1,
+                    "lang" => user.try(:language) || 'en',
+                    "menu" => 'false',
+                    "callback_url" => milestone_url(user_id: user.try(:id) || 0, script_level_id: script_level)}
   end
 
-  def build_level_url(level, user, callback_url = nil)
-    if !level.level_url.blank?
-      level.level_url
-    else
-      uri = URI.parse(level.game.base_url)
-      new_query_ar = uri.query ? URI.decode_www_form(uri.query) : []
-      new_query_ar << callback_url
-      new_query_ar << ["page", level.page] if level.page
-      new_query_ar << ["level", level.level_num || 1]
-      new_query_ar << ["skin", level.try(:skin) || 3]
-      new_query_ar << ["lang", user.try(:language) || 'en']
-      new_query_ar << ["menu", 'false']
-      uri.query = URI.encode_www_form(new_query_ar)
-      uri.to_s
-    end
-  end
-
-  def next_level_url(sl)
-    next_level = ScriptLevel.find_by_script_id_and_chapter(sl.script, sl.chapter + 1) || sl.script.script_levels.first
-    script_level_path(sl.script, next_level)
+  def script_level_url(script_level)
+    query_params = build_query_params(script_level, current_user)
+    script_level_path(script_level.script, script_level.level, query_params)
   end
 end
