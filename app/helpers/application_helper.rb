819a233d59d0f15013b2cfe72d1c3c6101f871af
Patrick O'Donnell <patrick@urbanspoon.com>
2013-09-27 15:30:51 -0700

    Add "mobile?" helper, and use it to avoid autoplaying videos and popup UI for any videos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
     s = s.gsub("a minute", "1 minute")
     "#{s} ago"
   end
+
+  def mobile?(agent = request.user_agent)
+    return true if agent =~ /\b(iPad|urbanpad)\b/
+    return true if agent =~ /BlackBerry|BB10.*mobile/i
+    return true if agent =~ /Android/
+    return true if agent =~ /\b(iPhone|iPod|CFNetwork)\b/
+    return true if agent =~ /Windows Phone/
+
+    #return true if agent =~ /^(?:ASTEL|AU-MIC|DoCoMo|J-PHONE|mot|Nokia|PDXGW|SEC|SonyEricsson|UPG1|Vodafone|Xiino)/i
+    #return true if agent =~ /\b(?:Android|AvantGo|Danger|DDIPOCKET|Elaine|embedix|maemo|MIDP|NetFront|nokia\d+|Opera Mini|Palm(OS|Source)|PlayStation|ProxiNet|RegKing|ReqwirelessWeb|SonyEricsson|Symbian ?OS|TELECA|Twitt[a-z]+|UP\.Browser|WinWAPDashMR|Windows CE|Pre)\b/i
+    false
+  end
 end
