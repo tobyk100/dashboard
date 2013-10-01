7ce7d9ae2a171585bad451402627cdd74771047e
Tobias Kahan <tobyk100@gmail.com>
2013-09-30 17:31:49 -0700

    Added cancan ability management.

--- app/controllers/callouts_controller.rb
+++ app/controllers/callouts_controller.rb
@@ -1,4 +1,6 @@
 class CalloutsController < ApplicationController
+  check_authorization
+  load_and_authorize_resource
   before_action :set_callout, only: [:show, :edit, :update, :destroy]
 
   # GET /callouts
