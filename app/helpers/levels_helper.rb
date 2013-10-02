93768215329f2fd09cdda519c7ab10ee8ea4f712
Brandon Bloom <brandon@brandonbloom.name>
2013-10-02 17:18:33 -0400

    Elimiante ignored query params

--- app/helpers/levels_helper.rb
+++ app/helpers/levels_helper.rb
@@ -1,18 +1,12 @@
 module LevelsHelper
-  def build_query_params(script_level, user)
-    level = script_level.level
-    skin = level.skin
-    {"level" => level.level_num || "1_1",
-     "skin" => skin}
+  def build_script_level_path(script_level)
+    script_level_path(script_level.script, script_level.level)
   end
 
-  def build_script_level_path(script_level)
-    query_params = build_query_params(script_level, current_user)
-    script_level_path(script_level.script, script_level.level, query_params)
-  end
   def language
     user.try(:language) || 'en'
   end
+
   def locale
     'en_us'
   end
