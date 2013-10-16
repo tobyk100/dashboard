e09aabc89f7c9d59331b5be06bf033d25a46ea1c
Patrick O'Donnell <patrick@urbanspoon.com>
2013-10-16 14:25:45 -0700

    Change tooltip messages, tweak padding

--- app/helpers/followers_helper.rb
+++ app/helpers/followers_helper.rb
@@ -2,7 +2,8 @@ module FollowersHelper
   def build_user_level_message(user_level, script_level)
     if user_level
       return <<TEXT
-#{script_level.level.game.name}: #{script_level.level.level_num}
+#{user_level.user.name}
+#{script_level.level.game.name} ##{script_level.game_chapter}
 Best result: #{user_level.best_result}
 Attempts: #{user_level.attempts}
 Last attempt: #{time_ago_in_words(user_level.updated_at)}
