41f02a0a25d9ad8bdadfaf3d0369a4377f0b2f07
Patrick O'Donnell <patrick@urbanspoon.com>
2013-10-16 14:31:28 -0700

    More tooltip changes

--- app/helpers/followers_helper.rb
+++ app/helpers/followers_helper.rb
@@ -1,8 +1,8 @@
 module FollowersHelper
-  def build_user_level_message(user_level, script_level)
+  def build_user_level_message(user, user_level, script_level)
     if user_level
       return <<TEXT
-#{user_level.user.name}
+#{user.name}
 #{script_level.level.game.name} ##{script_level.game_chapter}
 Best result: #{user_level.best_result}
 Attempts: #{user_level.attempts}
@@ -10,7 +10,7 @@ Last attempt: #{time_ago_in_words(user_level.updated_at)}
 First attempt: #{time_ago_in_words(user_level.created_at)}
 TEXT
     else
-      return "#{script_level.level.game.name}: #{script_level.level.level_num}\nNot attempted yet"
+      return "#{user.name} has not attempted #{script_level.level.game.name} ##{script_level.game_chapter}"
     end
   end
 end
