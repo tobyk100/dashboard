76be9653a284e7e3b6ea4eba726da686620dfacc
Brandon Bloom <brandon@brandonbloom.name>
2013-10-23 16:25:53 -0400

    Even more correcter path

--- server_setup.sh
+++ server_setup.sh
@@ -13,7 +13,7 @@ fi
 
 export DASH_ROOT=$1
 if [[ -z $DASH_ROOT ]]; then
-  export DASH_ROOT=/home/ubuntu/deploy/current
+  export DASH_ROOT=/home/ubuntu/apps/dashboard/current
 fi
 
 export CDO_USER=$2
