bc6c064a2ae1ffab1ce52f461045c9feb2821926
Brandon Bloom <brandon@brandonbloom.name>
2013-10-23 14:39:53 -0400

    Pseudolocalization
    - rake pseudolocalize
    - Added locale-setting around_filter to all routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   # this is needed to avoid devise breaking on email param
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  around_filter :with_locale
+
   protected
 
   PERMITTED_USER_FIELDS = [:name, :username, :email, :password, :password_confirmation, :language, :gender, :login, :remember_me, :birthday]
@@ -13,5 +15,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) do |u| u.permit PERMITTED_USER_FIELDS end
     devise_parameter_sanitizer.for(:sign_up) do |u| u.permit PERMITTED_USER_FIELDS end
     devise_parameter_sanitizer.for(:sign_in) do |u| u.permit PERMITTED_USER_FIELDS end
+  end
+
+  def with_locale
+    old_locale = I18n.locale
+    begin
+      if current_user
+        I18n.locale = current_user.locale
+      end
+      yield
+    ensure
+      I18n.locale = old_locale
+    end
   end
 end
