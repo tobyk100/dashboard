04098f2bf763d9e672a3ce8315c0b06a6af87d22
cpirich <chris@code.org>
2013-10-23 16:45:15 -0700

    [Finishes 59434970]: hide header on phones, but not iPad when playing through levels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,18 @@ module ApplicationHelper
     false
   end
 
+  def phone?(agent = request.user_agent)
+    # return true if agent =~ /\b(iPad|urbanpad)\b/
+    return true if agent =~ /BlackBerry|BB10.*mobile/i
+    return true if agent =~ /Android/
+    return true if agent =~ /\b(iPhone|iPod|CFNetwork)\b/
+    return true if agent =~ /Windows Phone/
+
+    #return true if agent =~ /^(?:ASTEL|AU-MIC|DoCoMo|J-PHONE|mot|Nokia|PDXGW|SEC|SonyEricsson|UPG1|Vodafone|Xiino)/i
+    #return true if agent =~ /\b(?:Android|AvantGo|Danger|DDIPOCKET|Elaine|embedix|maemo|MIDP|NetFront|nokia\d+|Opera Mini|Palm(OS|Source)|PlayStation|ProxiNet|RegKing|ReqwirelessWeb|SonyEricsson|Symbian ?OS|TELECA|Twitt[a-z]+|UP\.Browser|WinWAPDashMR|Windows CE|Pre)\b/i
+    false
+  end
+
   def youtube_url(code)
     "https://www.youtubeeducation.com/watch?v=#{code}"
   end
