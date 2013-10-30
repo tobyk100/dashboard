3146159d86be35188f087ef9c5e56c063fb8dcce
Tobias Kahan <tobyk100@gmail.com>
2013-10-30 11:34:11 -0700

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -148,8 +148,8 @@ ActiveRecord::Schema.define(version: 20131029225428) do
   add_index "user_trophies", ["user_id", "trophy_id", "concept_id"], name: "index_user_trophies_on_user_id_and_trophy_id_and_concept_id", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                               default: "",      null: false
-    t.string   "encrypted_password",                  default: "",      null: false
+    t.string   "email",                               default: "",        null: false
+    t.string   "encrypted_password",                  default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -166,10 +166,10 @@ ActiveRecord::Schema.define(version: 20131029225428) do
     t.boolean  "admin"
     t.string   "gender",                 limit: 1
     t.string   "name"
-    t.string   "locale",                 limit: 10,   default: "en-US", null: false
+    t.string   "locale",                 limit: 10,   default: "en-US",   null: false
     t.date     "birthday"
     t.string   "parent_email"
-    t.string   "user_type",              limit: 16
+    t.string   "user_type",              limit: 16,   default: "student"
     t.string   "school"
     t.string   "full_address",           limit: 1024
     t.string   "address"
