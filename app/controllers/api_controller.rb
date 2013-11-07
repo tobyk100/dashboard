8dc77e5afa821fb5467c014d7ae6191b24c82543
Patrick O'Donnell <patrick@urbanspoon.com>
2013-11-07 15:35:20 -0800

    Allow any domain to hit our user header api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,7 @@
 class ApiController < ApplicationController
   def user_menu
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
     render layout: false
   end
 end
