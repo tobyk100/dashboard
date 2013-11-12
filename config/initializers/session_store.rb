6b8c93ca61b591265550174b8f1ba20d37d6d468
Brandon Bloom <brandon@brandonbloom.name>
2013-11-12 14:09:21 -0500

    Rename cookie to break sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Dashboard::Application.config.session_store :cookie_store, key: '_dashboard_session', :httponly => false, domain: :all
+Dashboard::Application.config.session_store :cookie_store, key: '_learn_session', :httponly => false, domain: :all
