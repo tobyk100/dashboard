c3d3fa33f9a823f23cac6b3c848e8c75a382583c
Patrick O'Donnell <patrick@urbanspoon.com>
2013-11-18 08:54:24 -0800

    Up the default number of production unicorn procs

--- config/unicorn.rb.sh
+++ config/unicorn.rb.sh
@@ -3,7 +3,7 @@
 cat <<EOF
 listen 3000 # by default Unicorn listens on port 8080
 listen "/tmp/unicorn.dashboard.sock"
-worker_processes 2 # this should be >= nr_cpus
+worker_processes 8 # this should be >= nr_cpus
 pid "${DASH_ROOT}/tmp/pids/unicorn.pid"
 timeout 60
 stderr_path "/var/log/unicorn/stderr.log"
