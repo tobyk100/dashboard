04135beadbaf89d4b6117019d5070bd43f681a88
Brandon Bloom <brandon@brandonbloom.name>
2013-11-26 15:20:00 -0500

    ActivitiesController#prepare_response => ApplicationController#milestone_response

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -80,7 +80,7 @@ class ActivitiesController < ApplicationController
       end
     end
 
-    render json: prepare_response(script_level, total_lines, trophy_updates, solved)
+    render json: milestone_response(script_level, total_lines, trophy_updates, solved)
   end
 
   # GET /activities
@@ -235,56 +235,5 @@ class ActivitiesController < ApplicationController
     log_string += "\t#{request.user_agent}"
 
     milestone_logger.info log_string
-  end
-
-  def prepare_response(script_level, total_lines, trophy_updates, solved)
-    response = {}
-    level = script_level.level
-    # figure out the previous level
-    previous_level = script_level.previous_level
-    if previous_level
-      response[:previous_level] = build_script_level_path(previous_level)
-    end
-
-    # if they solved it, figure out next level
-    if solved
-      response[:total_lines] = total_lines
-
-      if (trophy_updates.length > 0)
-        response[:trophy_updates] = trophy_updates
-      end
-
-      next_level = script_level.next_level
-      # If this is the end of the current script
-      if !next_level
-        # If the current script is hour of code, continue on to twenty-hour
-        if script_level.script.hoc?
-          next_level = ScriptLevel.find_by_script_id_and_chapter(Script.find_twenty_hour_script.id, script_level.chapter + 1)
-          redirect = current_user ? build_script_level_path(next_level) : "http://code.org/api/hour/finish"
-        end
-        # Get the wrap up video
-        video = script_level.script.wrapup_video
-        response[:video_info] = { src: youtube_url(video.youtube_code),  key: video.key, name: data_t('video.name', video.key), redirect: redirect} if video
-        response[:message] = 'no more levels'
-      end
-      # Get the next_level setup
-      if next_level
-        response[:redirect] = build_script_level_path(next_level)
-
-        if (level.game_id != next_level.level.game_id)
-          response[:stage_changing] = {
-              previous: { number: level.game_id, name: level.game.name },
-              new: { number: next_level.level.game_id, name: next_level.level.game.name }
-          }
-        end
-
-        if (level.skin != next_level.level.skin)
-          response[:skin_changing] = { previous: level.skin, new: next_level.level.skin }
-        end
-      end
-    else
-      response[:message] = 'try again'
-    end
-    response
   end
 end
