4a794433def719272d07de7e2fd722e8ebf3841f
Patrick O'Donnell <patrick@urbanspoon.com>
2013-11-25 22:01:18 -0800

    Add new app servers to the production env.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,5 @@
 set :rails_env, 'production'
 server 'dash1.code.org', :app, :web, :db, :primary => true
+server 'dash2.code.org', :app, :web
+server 'dash3.code.org', :app, :web
+server 'dash4.code.org', :app, :web
