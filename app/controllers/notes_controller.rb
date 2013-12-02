25868ced9b7f292bc2ef198657d90d2115cb74ae
Brandon Bloom <brandon@brandonbloom.name>
2013-12-02 15:10:24 -0500

    Parallel @note_paths hash for notes views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,9 @@
 class NotesController < ApplicationController
   def index
     @notes = t('slides.' + params[:key])
+    # XXX Need to have a parallel "note_paths" hash for a variety of reasons.
+    # See config/locales/slides.en.yml for details.
+    @note_paths = t('slides.' + params[:key], locale: :en)
 
     render layout: false
   end
