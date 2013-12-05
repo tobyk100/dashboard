d6b7120563cb72161b8f3720ec474bed3e276fbf
Patrick O'Donnell <patrick@urbanspoon.com>
2013-12-04 16:06:07 -0800

    Lots of new staging servers!

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,9 @@ set :rails_env, 'staging'
 server "dash5.dev-code.org", :app, :web, :db, :primary => true
 server "dash6.dev-code.org", :app, :web
 server "dash7.dev-code.org", :app, :web
+server "dash8.dev-code.org", :app, :web
+server "dash9.dev-code.org", :app, :web
+server "dash10.dev-code.org", :app, :web
+server "dash11.dev-code.org", :app, :web
+server "dash12.dev-code.org", :app, :web
+server "dash13.dev-code.org", :app, :web
