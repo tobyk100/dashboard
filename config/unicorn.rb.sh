afc99488630d9212c824b8eb158c0d14e4651923
Patrick O'Donnell <patrick@urbanspoon.com>
2013-12-05 10:23:06 -0800

    Up unicorn backlog

--- config/unicorn.rb.sh
+++ config/unicorn.rb.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF
 listen 3000 # by default Unicorn listens on port 8080
-listen "/tmp/unicorn.dashboard.sock"
+listen "/tmp/unicorn.dashboard.sock", backlog: 8192
 worker_processes 32 # this should be >= nr_cpus
 pid "${DASH_ROOT}/tmp/pids/unicorn.pid"
 timeout 60
