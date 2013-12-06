f4ef9e75dd3a18d23d9fada834e1479a10bb9ead
Patrick O'Donnell <patrick@urbanspoon.com>
2013-12-05 21:56:13 -0800

    start shifting machines from staging to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,6 @@ server 'dash1.code.org', :app, :web, :db, :primary => true
 server 'dash2.code.org', :app, :web
 server 'dash3.code.org', :app, :web
 server 'dash4.code.org', :app, :web
+server 'dash5.code.org', :app, :web
+server 'dash6.code.org', :app, :web
+server 'dash7.code.org', :app, :web
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,9 @@ server "dash7.dev-code.org", :app, :web
 server "dash8.dev-code.org", :app, :web
 server "dash9.dev-code.org", :app, :web
 server "dash10.dev-code.org", :app, :web
-server "dash11.dev-code.org", :app, :web
 server "dash12.dev-code.org", :app, :web
 server "dash13.dev-code.org", :app, :web
+server "dash14.dev-code.org", :app, :web
+server "dash15.dev-code.org", :app, :web
+server "dash16.dev-code.org", :app, :web
+server "dash18.dev-code.org", :app, :web
