68248c04de9a83febef0f34ca97f0f8f7cf7d23b
Patrick O'Donnell <patrick@urbanspoon.com>
2013-12-06 13:06:26 -0800

    Remove stopped staging servers

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,10 +4,10 @@ server "dash6.dev-code.org", :app, :web
 server "dash7.dev-code.org", :app, :web
 server "dash8.dev-code.org", :app, :web
 server "dash9.dev-code.org", :app, :web
-server "dash10.dev-code.org", :app, :web
+#server "dash10.dev-code.org", :app, :web
 server "dash12.dev-code.org", :app, :web
 server "dash13.dev-code.org", :app, :web
-server "dash14.dev-code.org", :app, :web
-server "dash15.dev-code.org", :app, :web
+#server "dash14.dev-code.org", :app, :web
+#server "dash15.dev-code.org", :app, :web
 server "dash16.dev-code.org", :app, :web
 server "dash18.dev-code.org", :app, :web
