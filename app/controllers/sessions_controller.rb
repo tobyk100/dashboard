2ca19407815c7446c53217bf3e923d2b91131dc5
Nan <nan@code.org>
2013-12-05 16:54:11 -0800

    Added a clear flash message function.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,12 @@ class SessionsController < Devise::SessionsController
   # DELETE /resource/sign_out
   # Do not show the flash message after signing out.
   def destroy
-    redirect_path = after_sign_out_path_for(resource_name)
-    yield resource if block_given?
+    super
+    clear_flash_message :notice
+  end
 
-    # We actually need to hardcode this as Rails default responder doesn't
-    # support returning empty response on GET request
-    respond_to do |format|
-      format.all { head :no_content }
-      format.any(*navigational_formats) { redirect_to redirect_path }
-    end
+  def clear_flash_message(key)
+    flash[key] = ''
   end
 
 end
