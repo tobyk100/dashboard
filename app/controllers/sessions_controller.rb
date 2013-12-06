c3b6f9a7e5214fd7a358e59f3311f25e7373a3c9
Nan <nan@code.org>
2013-12-05 16:23:54 -0800

    Removed the sign out message since we are redirecting now.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,18 @@ class SessionsController < Devise::SessionsController
 
   before_filter :nonminimal
 
+  # DELETE /resource/sign_out
+  # Do not show the flash message after signing out.
+  def destroy
+    redirect_path = after_sign_out_path_for(resource_name)
+    yield resource if block_given?
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to redirect_path }
+    end
+  end
+
 end
