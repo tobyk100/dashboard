9d54b7e8421b3ffc8959649ff372f506a55722e3
Patrick O'Donnell <patrick@urbanspoon.com>
2013-12-10 09:15:50 -0800

    Scale down staging env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,2 @@
 set :rails_env, 'staging'
-server "dash3.dev-code.org", :app, :web
-server "dash5.dev-code.org", :app, :web, :db, :primary => true
-server "dash6.dev-code.org", :app, :web
-server "dash7.dev-code.org", :app, :web
-server "dash8.dev-code.org", :app, :web
-server "dash12.dev-code.org", :app, :web
+server "dash4.dev-code.org", :app, :web, :db, :primary => true
