414587464ebcb41e9cb6648642f112884dbd8fcf
Tobias Kahan <tobyk100@gmail.com>
2013-12-11 17:11:35 -0800

    Toggle sharing and hiding source passed on dashboard params.

--- app/controllers/level_sources_controller.rb
+++ app/controllers/level_sources_controller.rb
@@ -1,15 +1,18 @@
 class LevelSourcesController < ApplicationController
   include LevelsHelper
 
-  def show
+  def show(hide_source=true)
     @level_source = LevelSource.find(params[:id])
     @level = @level_source.level
     @game = @level.game
     @full_width = true
+    @hide_source = hide_source
+    @share = true
   end
 
   def edit
     # currently edit is the same as show...
-    show
+    show false
+    render "show"
   end
 end
