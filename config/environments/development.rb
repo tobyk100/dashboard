c77c399fb3cc436a77e8827e49c4b2c647a33d5a
Tobias Kahan <tobyk100@gmail.com>
2013-12-13 09:22:31 -0800

    Use rails env for default pretty print blockly.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,4 +30,7 @@ Dashboard::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Whether or not to display pretty blockly.
+  config.pretty_blockly = true
 end
