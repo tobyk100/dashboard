08397caae89bced7036693002e37036c5e048acb
Nan <nan@code.org>
2013-12-12 16:14:08 -0800

    Added level source into response for sharing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -87,6 +87,11 @@ class ApplicationController < ActionController::Base
     else
       response[:message] = 'try again'
     end
+
+    if options[:level_source]
+      response[:level_source] = "http://" + request.host_with_port + level_source_path(options[:level_source])
+    end
+
     response
   end
 
