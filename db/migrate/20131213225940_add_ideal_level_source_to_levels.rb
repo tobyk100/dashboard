c90149eb30907180d2c9d1c2f561c654a5982d4c
Tobias Kahan <tobyk100@gmail.com>
2013-12-13 16:31:23 -0800

    Add ideal source to level edit form.

--- db/migrate/20131213225940_add_ideal_level_source_to_levels.rb
+++ db/migrate/20131213225940_add_ideal_level_source_to_levels.rb
@@ -1,5 +1,9 @@
 class AddIdealLevelSourceToLevels < ActiveRecord::Migration
   def change
     add_column :levels, :ideal_level_source_id, :integer
+    Level.all.map do |level|
+      best = Activity.where(level_id: level.id, test_result: 100).group(:level_source_id).max
+      level.update_attributes(level_source_id: best.level_source) if best
+    end
   end
 end
