afef020b4ebc85b40a388c5813deb59d7060dabc
Patrick O'Donnell <patrick@urbanspoon.com>
2013-12-16 15:40:29 -0800

    Update production server list

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,14 +5,9 @@ server 'dash6.code.org', :app, :web
 server 'dash7.code.org', :app, :web
 server 'dash8.code.org', :app, :web
 server 'dash9.code.org', :app, :web
-server 'dash10.code.org', :app, :web
-server 'dash11.code.org', :app, :web
-server 'dash12.code.org', :app, :web
 server 'dash13.code.org', :app, :web
-server 'dash14.code.org', :app, :web
 server 'dash15.code.org', :app, :web
 server 'dash16.code.org', :app, :web
-server 'dash17.code.org', :app, :web
 server 'dash19.code.org', :app, :web
 server 'dash20.code.org', :app, :web
 server 'dash23.code.org', :app, :web
