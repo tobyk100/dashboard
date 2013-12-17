fdaf731a782eb0fa3ba39ff38fabd623401fda2e
Nan <nan@code.org>
2013-12-17 15:55:52 -0800

    Serving the image data as a url.

--- app/controllers/level_sources_controller.rb
+++ app/controllers/level_sources_controller.rb
@@ -14,7 +14,12 @@ class LevelSourcesController < ApplicationController
 
   def show_image
     @level_source = LevelSource.find(params[:id])
-    image = @level_source.image.nil? ? ActionController::Base.helpers.asset_url('sharing_drawing.png') : @level_source.image
+    @level_source.image.nil? ? ActionController::Base.helpers.asset_url('sharing_drawing.png') : url_for(:controller => "level_sources", :action => "generate_image", :id => params[:id], only_path: false)
+  end
+
+  def generate_image
+    @level_source = LevelSource.find(params[:id])
+    send_data @level_source.image, :type => 'image/png', :disposition => 'inline'
   end
 
   protected
