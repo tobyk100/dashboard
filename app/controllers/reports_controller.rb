a75a3c5a85b65ffd4f1e830d16d15d4661333377
cpirich <chris@code.org>
2013-12-17 11:13:28 -0800

    WIP: admin stats

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   before_filter :authenticate_user!, except: [:header_stats]
-  check_authorization except: [:all_usage, :level_stats, :students, :header_stats]
+  check_authorization except: [:all_usage, :level_stats, :students, :header_stats, :admin_stats]
 
   before_action :set_script
 
@@ -45,6 +45,32 @@ SQL
 
     @recent_activities = get_base_usage_activity
     render 'usage', formats: [:html]
+  end
+
+  def admin_stats
+    raise "unauthorized" if !current_user.admin?
+    
+    SeamlessDatabasePool.use_persistent_read_connection do
+      @all_script_levels = Script.twenty_hour_script.script_levels.includes({ level: :game })
+ 
+      @user_count = User.count
+      @teacher_count = User.where(:user_type => 'teacher').count
+      @student_count = @user_count - @teacher_count
+      @users_with_teachers = Follower.distinct.count(:student_user_id)
+      @users_with_email = User.where('email <> ""').count
+ 
+      @prizes_redeemed = Prize.where('user_id IS NOT NULL').group(:prize_provider).count
+      @prizes_available = Prize.where('user_id IS NULL').group(:prize_provider).count
+      
+      @student_prizes_earned = User.where(:prize_earned => true).count
+      @student_prizes_redeemed = Prize.where('user_id IS NOT NULL').count
+      @student_prizes_available = Prize.where('user_id IS NULL').count
+
+      @teacher_prizes_earned = User.where(:teacher_prize_earned => true).count
+      
+      @teacher_bonus_prizes_earned = User.where(:teacher_bonus_prize_earned => true).count
+
+    end
   end
 
   def students
