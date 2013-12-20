da8dbfb9e8a2e6cef9e1d91257f798da53e16210
Nan <nan@code.org>
2013-12-19 22:05:38 -0800

    Moved level source image to a separate table.

--- app/controllers/level_sources_controller.rb
+++ app/controllers/level_sources_controller.rb
@@ -15,18 +15,19 @@ class LevelSourcesController < ApplicationController
   end
 
   def show_image
-    @level_source = LevelSource.find(params[:id])
-    if @level_source.image.nil?
-      request.protocol + request.host_with_port + ActionController::Base.helpers.asset_path('sharing_drawing.png')
-    else
+    level_source = LevelSource.find(params[:id])
+    level_source_image = LevelSourceImage.find_by_level_source_id(level_source.id)
+    if !level_source_image.nil? && !level_source_image.image.nil?
       url_for(:controller => "level_sources", :action => "generate_image", :id => params[:id], only_path: false)
+    else
+      request.protocol + request.host_with_port + ActionController::Base.helpers.asset_path('sharing_drawing.png')
     end
   end
 
   def generate_image
     background_url = 'app/assets/images/blank_sharing_drawing.png'
-    level_source = LevelSource.find(params[:id])
-    drawing_blob = level_source.image
+    level_source_id = LevelSource.find(params[:id]).id
+    drawing_blob = LevelSourceImage.find(level_source_id).image
     drawing_on_background = ImageLib::overlay_image(:background_url => background_url, :foreground_blob => drawing_blob)
     send_data drawing_on_background.to_blob, :stream => 'false', :type => 'image/png', :disposition => 'inline'
   end
---
+++ app/models/level_source_image.rb
@@ -0,0 +1,2 @@
+class LevelSourceImage < ActiveRecord::Base
+end
